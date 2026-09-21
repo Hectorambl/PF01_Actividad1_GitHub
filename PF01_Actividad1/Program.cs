4f8aab77e46dd6d0a96f1b3cfb49d9d99744ce8d
hectorambit <hectorambit04@gmail.com>
2026-09-21 19:26:58 +0200

    Primer commit: proyecto inicial

---
+++ PF01_Actividad1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PF01_Actividad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
