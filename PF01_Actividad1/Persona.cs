ece18b52bacc9df08c832505d62df82609ed9028
hectorambit <hectorambit04@gmail.com>
2026-09-22 19:31:12 +0200

    Func1: crear clase Persona Desarrollador 1 Hector

---
+++ PF01_Actividad1/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PF01_Actividad1
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
