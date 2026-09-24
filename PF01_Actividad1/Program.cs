4a766f89a11b7ceb9c9d9a34c93a5b97ecd462f7
hectorambit <hectorambit04@gmail.com>
2026-09-24 13:45:34 +0200

    Func2: llamar a ModificarNombre desde Main Desarrollador 1 Hector

--- PF01_Actividad1/Program.cs
+++ PF01_Actividad1/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Persona pers = new Persona("Marcos", 22);
+            pers.MostrarDatos();
+
+            pers.ModificarNombre("Carlos");
+            pers.MostrarDatos();
         }
     }
 }
